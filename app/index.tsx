afdf5a8c6e889404e2f501ca5faaef69dc48fa1a
bachld <ldbach97@gmail.com>
2025-04-27 00:47:37 +0700

    Add bell sound to initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -13,14 +13,55 @@ import { useRouter } from "expo-router";
 import { Colors } from '@/constants/Colors';
 
 import { EntranceAnimatorWrap } from "@/components/EntranceAnimatorWrap";
+import { useEffect } from "react";
+
+import { Audio } from 'expo-av';
+import { Sound} from "expo-av/build/Audio";
+
+const audioSource = require('@/assets/bell.mp3');
+
 
 export default function Index() {
-
   const buddhaImage = require('@/assets/splash-icon2.png')
-  // const buddhaImage = require('@/assets/images/splash-icon.png')
 
   const insets = useSafeAreaInsets();
   const router = useRouter();
+
+  const playSound = async () => {
+    let soundObj = await Audio.Sound.createAsync(
+      audioSource
+    )
+    const sound = soundObj.sound
+    await sound.setVolumeAsync(0.5)
+    await sound.playAsync()
+
+    setTimeout(async () => {
+      await fadeOut(sound); // Fade out after 3 seconds
+    }, 3000);
+  }
+
+  async function fadeOut(soundObject: Sound, duration = 1000) {
+    if (!soundObject) return;
+  
+    const initialVolume = 0.5
+    const steps = 20;
+    const delay = duration / steps;
+  
+    for (let i = steps; i >= 0; i--) {
+      const volume = initialVolume * (i / steps);
+      await soundObject.setVolumeAsync(volume);
+      await new Promise(resolve => setTimeout(resolve, delay));
+    }
+    await soundObject.pauseAsync();
+    await soundObject.setPositionAsync(0); // Optional: reset to beginning
+    await soundObject.unloadAsync()
+  }
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await playSound()
+    }, 500); 
+  }, [])
 
   return (
     <ImageBackground source={buddhaImage}
